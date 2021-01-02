5f447bc93f7ae929092c1ec11208c8550355264d
Alfonso <anroswell@gmail.com>
2021-01-02 11:42:10 -0500

    Primer commit

---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ErrorComponent } from './pages/error/error.component';
+
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AdminComponent } from './pages/admin/admin.component';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { CompanyFormComponent } from './pages/admin/settings/company/company-form.component';
+
+@NgModule({
+  declarations: [AppComponent, LoginComponent, ErrorComponent, AdminComponent, CompanyFormComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
